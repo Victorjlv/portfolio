0355e59d0f19a67f9571e51d287ca241f0b4b07f
João Victor <lockeff16@gmail.com>
2023-01-31 15:32:33 -0300

    portfolio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import initAnimationScroll from "./modulos/animacao-scroll.js";
+import initMenuMobile from "./modulos/mobile.js";
+import initMenuTransition from "./modulos/transition.js";
+import initScrollSuave from "./modulos/scroll-suave.js";
+import initTabNav from "./modulos/tabnav.js";
+import initTyping from "./modulos/typing.js";
+
+
+
+initTabNav();
+initScrollSuave();
+initAnimationScroll();
+initTyping();
+initMenuTransition();
+initMenuMobile();
